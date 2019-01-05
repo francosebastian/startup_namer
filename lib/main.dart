0859e273f03c8d924cb0d9aafe239e77d02e2448
francosebastian <francosebastian22@gmail.com>
2019-01-05 13:07:20 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+/*
+  This example creates a Material app.
+  Material is a visual design language that is standard on mobile and the web.
+  Flutter offers a rich set of Material widgets.
+ */
+import 'package:flutter/material.dart';
+
+//Import for random english word library
+import 'package:english_words/english_words.dart';
+
+/*
+  The main() method uses arrow (=>) notation.
+  Use arrow notation for one-line functions or methods.
+*/
+void main() => runApp(MyApp());
+
+/*
+  The app extends StatelessWidget which makes the app itself a widget. 
+  In Flutter, almost everything is a widget, including alignment, padding, 
+  and layout.
+ */
+class MyApp extends StatelessWidget {
+  /*
+  A widget’s main job is to provide a build() method that describes how to
+  display the widget in terms of other, lower level widgets.
+   */
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  /*Note: Prefixing an identifier with an underscore enforces privacy
+   in the Dart language. */
+  final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>(); // Add this line.
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    /*The Scaffold widget, from the Material library,
+       provides a default app bar, title,
+       and a body property that holds the widget tree for the home screen.
+       The widget subtree can be quite complex. */
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider();
+          /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair); // Add this
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
